6d7054d9ee606a62acd75dfaad899af11f59df5a
UponShadows <uponshadows2@gmail.com>
2022-12-22 14:59:00 +0200

    Second Set of Migrations ... Done!

--- base/core/models.py
+++ base/core/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    # username = None # Removes username field so u login with email 
-    # email = models.EmailField(max_length=256, unique=True)
+    username = None # Removes username field so u login with email 
+    email = models.EmailField(max_length=256, unique=True)
     
     
     
     objects = UserManager() 
-    # REQUIRED_FIELDS = []
-    # USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
     pass
